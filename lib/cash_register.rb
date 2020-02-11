f05532c28565cace5ec19e36a52cc32716a03d5d
Daniel Sun <danielsun12345@gmail.com>
2020-02-11 02:55:42 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,31 +1,40 @@
 class CashRegister
+  
+  attr_accessor :total, :discount, :items, :last_amount
 
-  attr_accessor :items, :discount, :total, :last_transaction
-
-  def initialize(discount=0)
-    @total = 0
+  def initialize(discount = 0.0)
+    @total = 0 
     @discount = discount
     @items = []
   end
-
-  def add_item(title, amount, quantity=1)
-    self.total += amount * quantity
-    quantity.times do
-      items << title
-    end
-    self.last_transaction = amount * quantity
+  
+  def add_item(item, cost, quantity = 1)
+    if quantity > 1 
+      i = 0 
+      while i < quantity
+        @items << item
+        i += 1 
+      end 
+    else 
+      @items << item
+    end 
+    @total += quantity*cost
+    @last_amount = @total
+    @total
   end
-
-  def apply_discount
-    if discount != 0
-      self.total = (total * ((100.0 - discount.to_f)/100)).to_i
-      "After the discount, the total comes to $#{self.total}."
-    else
+  
+  def apply_discount()
+    if @discount > 0 
+      @discount = @discount/100.to_f 
+      @total = @total - (@total*(@discount))
+      "After the discount, the total comes to $#{@total.to_i}."
+    else 
       "There is no discount to apply."
-    end
+    end 
+  end 
+  
+  def void_last_transaction()
+    @total = @total - @last_amount 
   end
-
-  def void_last_transaction
-    self.total = self.total - self.last_transaction
-  end
+  
 end

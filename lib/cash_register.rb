65bc96fd723d4e6a83a0a9eeb03cad66f8e82993
Daniel Sun <danielsun12345@gmail.com>
2020-02-11 02:38:12 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,21 @@
+class CashRegister
+  
+  attr_accessor :total, :discount, :item, :last_amount
 
+  def initialize(discount = 0.0)
+    @total = 0 
+    @discount = discount
+  end
+  
+  def add_item(item, cost, quantity = 0)
+    if quantity > 1 
+      i = 0 
+      while 
+      
+    
+    
+  end
+  
+  
+  
+end
